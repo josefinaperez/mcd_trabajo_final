546e1cc87b40fc72b41cde0598c3c88603710f54
Josefina Perez <josefinaperez@MacBook-Pro-de-Josefina.local>
2026-01-04 19:02:40 -0300

    pipe observaciones y vars wordclim eda

--- src/config.py
+++ src/config.py
@@ -1,1 +1,11 @@
 MAX_POSITIONAL_ACCURACY = 5000
+MIN_DATE = "1950-01-01"
+ARG_SHP_PATH = "../data/shp/argentina/argentina.shp"
+STA_FE_SHP_PATH = "../data/shp/santa_fe/santa_fe.shp"
+
+import os
+
+# Opción A: Configurarlas directamente en la sesión actual de Python
+os.environ['GBIF_USER'] = "josefinap92"
+os.environ['GBIF_PWD'] = "7xYsuN9L9atpB#q"
+os.environ['GBIF_EMAIL'] = "josefinap92@gmail.com"
---
+++ src/utils/plot.py
@@ -0,0 +1,57 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import pandas as pd
+from rasterio.mask import mask
+import rasterio
+
+def plot_points(shape_path, df_path):
+    shape = gpd.read_file(shape_path)
+    shape = shape.to_crs("EPSG:4326")
+    df = pd.read_csv(df_path)
+    
+    gdf_points = gpd.GeoDataFrame(
+        df,
+        geometry=gpd.points_from_xy(df["decimalLongitude"], df["decimalLatitude"]),
+        crs="EPSG:4326"
+    )
+    
+    mask = gdf_points.within(shape.union_all())
+    gdf_points_within = gdf_points[mask]
+    
+    ax = shape.plot(
+        figsize=(6, 10),
+        color="white",
+        edgecolor="black"
+    )
+
+    gdf_points_within.plot(
+        ax=ax,
+        color="red",
+        markersize=5
+    )
+    
+    plt.axis("off")
+    plt.show()
+
+
+def plot_raster(shape_path, wc_path, title=""):
+
+    shp = gpd.read_file(shape_path).to_crs("EPSG:4326")
+
+    with rasterio.open(wc_path) as src:
+        bio, transform = mask(
+        src,
+        shp.geometry,
+        crop=True,
+        filled=False
+    )
+
+
+    bio = bio[0]
+    
+    plt.figure(figsize=(3, 4))
+    plt.imshow(bio)
+    plt.colorbar(label=title)
+    plt.title(title)
+    plt.axis("off")
+    plt.show()
